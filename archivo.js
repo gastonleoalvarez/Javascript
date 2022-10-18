d45f2142072a9982c10c885b3faab0d579740ceb
gaston <leo_alva@hotmail.com>
2022-10-18 20:46:59 -0300

    segunda entrega

--- archivo.js
+++ archivo.js
@@ -1,19 +1,71 @@
-function guardar(){
-    function Persona(nombre,apellido){
-        this.nombre=nombre;
-        this.apellido=apellido;
-    }
-    var nombreCapturar= document.getElementById("nombre").value;
-    console.log(nombreCapturar);
-    var apellidoCapturar= document.getElementById("apellido").value;
+const productos = [
+{nombre: "yerba", precio: 110},
+{nombre: "harina", precio: 100},
+{nombre: "azucar", precio: 70},
+{nombre: "cafe", precio: 150},
+{nombre: "galletitas", precio:120},
+];
 
-    nuevoSujeto = new Persona(nombreCapturar, apellidoCapturar);
-    agregar();
+let carrito = []
+
+let seleccion = prompt ("quire comprar algun producto si o no")
+
+while (seleccion != "si" && seleccion != "no"){
+    alert ("por favor ingresa si o no")
+    seleccion = prompt("hola desea comprar algo si o no")
 }
 
-var datos = [];
-function agregar(){
-    datos.push(nuevoSujeto);
-    console.log(datos);
-    document.getElementById("tabla").innerHTML += '<tbody><td>'+nuevoSujeto.nombre+'</td><td>'+nuevoSujeto.apellido+'</td></tbody>'; 
-};
+if (seleccion == "si"){
+    alert ("lista de productos")
+    let todoslosProductos = productos.map((producto) => producto.nombre+ " " + producto.precio + "$");
+    alert(todoslosProductos.join(" - "))
+}else if (selccion =="no"){
+    alert ("gracias por venir")
+}
+
+while(seleccion != "no"){
+    let producto = prompt ("agregar un producto a tu carrito")
+    let precio = 0
+
+    if(producto == "yerba" || producto == "harina" || producto == "azucar" || producto == "cafe" || producto == "galletitas"){
+        switch(producto){
+            case "yerba":
+            precio = 110;
+            break;
+            case "harina":
+            precio = 100;
+            break;
+            case "azucar":
+            precio = 70;
+            break;
+            case "cafe":
+            precio = 150;
+            break;
+            case "galletitas":
+            precio = 120;
+            break;
+            default:
+                break;
+            
+        }
+    let unidades = parseInt(prompt("cuantas unidades quiere llevar"))
+
+    carrito.push({producto, unidades, precio})
+    console.log(carrito)
+    }else {
+        alert ("producto no encontrado")
+    }
+
+    seleccion = prompt ("desea seguir comprando?")
+
+    while (seleccion === "no"){
+        alert("gracias por la compra")
+        carrito.forEach((carritoFinal) => {
+            console.log(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades},total a pagar por producto ${carritoFinal.unidades * carritoFinal.precio}`)
+        })
+    break; 
+    }
+}
+
+const total = carrito.reduce((ecc, el) => acc + el.precio * el.unidades, 0)
+console.log(`total a pagar: ${total}`)
