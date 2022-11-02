9974c5edc77991797ea96a84c9ec96f6a43b95ed
gaston <leo_alva@hotmail.com>
2022-11-01 23:00:42 -0300

    agrego archivos

---
+++ archivo2.js
@@ -0,0 +1,70 @@
+const contenido= document.getElementById("contenido");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
+let carrito = [];
+
+productos.forEach((product)=> {
+    let content = document.createElement("div");
+    content.className ="card";
+    content.innerHTML= ` 
+    <img src="${product.img}">
+    <h3>${product.nombre}</h3>
+    <p class="parrafo">${product.precio}$</p>
+    `;
+
+    contenido.append(content);
+
+    let comprar =document.createElement("button")
+    comprar.innerText = "comprar";
+    comprar.className = "comprar";
+
+    content.append(comprar);
+
+    comprar.addEventListener("click", () => {
+        carrito.push({
+            id: product.id,
+            img: product.img,
+            nombre: product.nombre,
+            precio: product.precio,
+        })
+    })
+});
+
+verCarrito.addEventListener("click", () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header"
+    modalHeader.innerHTML = `
+    <h1 class ="modal-header-title">Carrito.</h1>
+    `;
+    modalContainer.append(modalHeader);
+
+    const modalbutton = document.createElement("h1");
+    modalbutton.innerText = "x";
+    modalbutton.className = "modal-header-button";
+
+    modalbutton.addEventListener("click", () => {
+        modalContainer.style.display = "none";
+    });
+
+    modalHeader.append(modalbutton);
+
+    carrito.forEach((product) => {
+        let carritoContent = document.createElement("div")
+        carritoContent.className = "modal-content"
+        carrito.innerHTML = `
+            <img src="${product.img}">
+            <h3>${product.nombre}</h3>
+            <p>${product.precio} $ </p>  
+        `;
+        modalContainer.append(carritoContent);
+    }); 
+    
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+
+    const totalBuying = document.createElement("div")
+    totalBuying.className = "total-content"
+    totalBuying.innerHTML = `total a pagar": ${total} $`;
+    modalContainer.append(totalBuying);
+});
---
+++ productos.js
@@ -0,0 +1,33 @@
+const productos = [    
+    {
+        id: 1,
+        nombre: "yerba", 
+        precio: 110,
+        img: 
+        "https://www.conradomarket.com.ar/images/000000000000011362136ALMACEN-yerba-PLAYADITO-x-500.jpg",
+    },
+    {
+        id: 2,
+        nombre: "harina", 
+        precio: 100,
+        img: "https://carrefourar.vtexassets.com/arquivos/ids/205215/7792180139313_02.jpg?v=637601556417500000",
+    },
+    {
+        id: 3,
+        nombre: "azucar", 
+        precio: 70,
+        img: "https://www.casa-segal.com/wp-content/uploads/2019/03/azucar-kilo-ledesma-reposteria-mendoza-casa-segal-1-600x600.jpg",
+    },
+    {
+        id: 4,
+        nombre: "cafe", 
+        precio: 150,
+        img: "https://www.rimoldimayorista.com.ar/datos/uploads/mod_catalogo/31308/nescafe-dolca-170g-6041235a4b2f6.jpg",
+    },
+    {
+        id: 5,
+        nombre: "galletitas", 
+        precio: 120,
+        img: "http://d3ugyf2ht6aenh.cloudfront.net/stores/001/279/512/products/bagley-variedad1-9dc80cfd4c02ca926116010751438649-640-0.jpg",
+    },
+];
